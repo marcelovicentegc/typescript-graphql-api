83b1d2989db58eef7bcb29746cded2b6c26fae7d
Marcelo Cardoso <marcelovicentegc@gmail.com>
2020-12-13 20:37:30 -0300

    chore: update gen files

---
+++ src/schema/graphql/generated.ts
@@ -0,0 +1,184 @@
+import { GraphQLResolveInfo } from 'graphql';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Animal = {
+  __typename?: 'Animal';
+  id: Scalars['ID'];
+  species: Scalars['String'];
+  favoriteFood: Scalars['String'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createAnimal: Animal;
+  updateAnimal: Scalars['Boolean'];
+  deleteAnimal: Scalars['Boolean'];
+};
+
+
+export type MutationCreateAnimalArgs = {
+  species: Scalars['String'];
+  favoriteFood: Scalars['String'];
+};
+
+
+export type MutationUpdateAnimalArgs = {
+  id: Scalars['ID'];
+  species: Scalars['String'];
+  favoriteFood: Scalars['String'];
+};
+
+
+export type MutationDeleteAnimalArgs = {
+  id: Scalars['ID'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  animal: Animal;
+  animals: Array<Animal>;
+};
+
+
+export type QueryAnimalArgs = {
+  id: Scalars['ID'];
+};
+
+
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
+
+export type LegacyStitchingResolver<TResult, TParent, TContext, TArgs> = {
+  fragment: string;
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+
+export type NewStitchingResolver<TResult, TParent, TContext, TArgs> = {
+  selectionSet: string;
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+export type StitchingResolver<TResult, TParent, TContext, TArgs> = LegacyStitchingResolver<TResult, TParent, TContext, TArgs> | NewStitchingResolver<TResult, TParent, TContext, TArgs>;
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
+  | ResolverFn<TResult, TParent, TContext, TArgs>
+  | StitchingResolver<TResult, TParent, TContext, TArgs>;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+}
+
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+}
+
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
+
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = {
+  Query: ResolverTypeWrapper<{}>;
+  ID: ResolverTypeWrapper<Scalars['ID']>;
+  Animal: ResolverTypeWrapper<Animal>;
+  String: ResolverTypeWrapper<Scalars['String']>;
+  Mutation: ResolverTypeWrapper<{}>;
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+};
+
+/** Mapping between all available schema types and the resolvers parents */
+export type ResolversParentTypes = {
+  Query: {};
+  ID: Scalars['ID'];
+  Animal: Animal;
+  String: Scalars['String'];
+  Mutation: {};
+  Boolean: Scalars['Boolean'];
+};
+
+export type AnimalResolvers<ContextType = any, ParentType extends ResolversParentTypes['Animal'] = ResolversParentTypes['Animal']> = {
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  species?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  favoriteFood?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
+  createAnimal?: Resolver<ResolversTypes['Animal'], ParentType, ContextType, RequireFields<MutationCreateAnimalArgs, 'species' | 'favoriteFood'>>;
+  updateAnimal?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType, RequireFields<MutationUpdateAnimalArgs, 'id' | 'species' | 'favoriteFood'>>;
+  deleteAnimal?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType, RequireFields<MutationDeleteAnimalArgs, 'id'>>;
+};
+
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  animal?: Resolver<ResolversTypes['Animal'], ParentType, ContextType, RequireFields<QueryAnimalArgs, 'id'>>;
+  animals?: Resolver<Array<ResolversTypes['Animal']>, ParentType, ContextType>;
+};
+
+export type Resolvers<ContextType = any> = {
+  Animal?: AnimalResolvers<ContextType>;
+  Mutation?: MutationResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
+};
+
+
+/**
+ * @deprecated
+ * Use "Resolvers" root object instead. If you wish to get "IResolvers", add "typesPrefix: I" to your config.
+ */
+export type IResolvers<ContextType = any> = Resolvers<ContextType>;
--- src/targets/web.ts
+++ src/targets/web.ts
@@ -1,52 +1,232 @@
+import gql from 'graphql-tag';
+import * as ApolloReactCommon from '@apollo/react-common';
+import * as ApolloReactHoc from '@apollo/react-hoc';
 export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string,
-  String: string,
-  Boolean: boolean,
-  Int: number,
-  Float: number,
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
 };
 
 export type Animal = {
-   __typename?: 'Animal',
-  id: Scalars['ID'],
-  species: Scalars['String'],
-  favoriteFood: Scalars['String'],
+  __typename?: 'Animal';
+  id: Scalars['ID'];
+  species: Scalars['String'];
+  favoriteFood: Scalars['String'];
 };
 
 export type Mutation = {
-   __typename?: 'Mutation',
-  createAnimal: Animal,
-  updateAnimal: Scalars['Boolean'],
-  deleteAnimal: Scalars['Boolean'],
+  __typename?: 'Mutation';
+  createAnimal: Animal;
+  updateAnimal: Scalars['Boolean'];
+  deleteAnimal: Scalars['Boolean'];
 };
 
 
 export type MutationCreateAnimalArgs = {
-  species: Scalars['String'],
-  favoriteFood: Scalars['String']
+  species: Scalars['String'];
+  favoriteFood: Scalars['String'];
 };
 
 
 export type MutationUpdateAnimalArgs = {
-  id: Scalars['ID'],
-  species: Scalars['String'],
-  favoriteFood: Scalars['String']
+  id: Scalars['ID'];
+  species: Scalars['String'];
+  favoriteFood: Scalars['String'];
 };
 
 
 export type MutationDeleteAnimalArgs = {
-  id: Scalars['ID']
+  id: Scalars['ID'];
 };
 
 export type Query = {
-   __typename?: 'Query',
-  animal: Animal,
-  animals: Array<Animal>,
+  __typename?: 'Query';
+  animal: Animal;
+  animals: Array<Animal>;
 };
 
 
 export type QueryAnimalArgs = {
-  id: Scalars['ID']
+  id: Scalars['ID'];
 };
+
+export type CreateAnimalMutationVariables = Exact<{
+  species: Scalars['String'];
+  favoriteFood: Scalars['String'];
+}>;
+
+
+export type CreateAnimalMutation = (
+  { __typename?: 'Mutation' }
+  & { createAnimal: (
+    { __typename?: 'Animal' }
+    & Pick<Animal, 'species' | 'favoriteFood'>
+  ) }
+);
+
+export type UpdateAnimalMutationVariables = Exact<{
+  id: Scalars['ID'];
+  species: Scalars['String'];
+  favoriteFood: Scalars['String'];
+}>;
+
+
+export type UpdateAnimalMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'updateAnimal'>
+);
+
+export type DeleteAnimalMutationVariables = Exact<{
+  id: Scalars['ID'];
+}>;
+
+
+export type DeleteAnimalMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'deleteAnimal'>
+);
+
+export type GetAnimalQueryVariables = Exact<{
+  id: Scalars['ID'];
+}>;
+
+
+export type GetAnimalQuery = (
+  { __typename?: 'Query' }
+  & { animal: (
+    { __typename?: 'Animal' }
+    & Pick<Animal, 'id' | 'species' | 'favoriteFood'>
+  ) }
+);
+
+export type GetAnimalsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetAnimalsQuery = (
+  { __typename?: 'Query' }
+  & { animals: Array<(
+    { __typename?: 'Animal' }
+    & Pick<Animal, 'id' | 'species' | 'favoriteFood'>
+  )> }
+);
+
+
+export const CreateAnimalDocument = gql`
+    mutation CreateAnimal($species: String!, $favoriteFood: String!) {
+  createAnimal(species: $species, favoriteFood: $favoriteFood) {
+    species
+    favoriteFood
+  }
+}
+    `;
+export type CreateAnimalMutationFn = ApolloReactCommon.MutationFunction<CreateAnimalMutation, CreateAnimalMutationVariables>;
+export type CreateAnimalProps<TChildProps = {}, TDataName extends string = 'mutate'> = {
+      [key in TDataName]: ApolloReactCommon.MutationFunction<CreateAnimalMutation, CreateAnimalMutationVariables>
+    } & TChildProps;
+export function withCreateAnimal<TProps, TChildProps = {}, TDataName extends string = 'mutate'>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  CreateAnimalMutation,
+  CreateAnimalMutationVariables,
+  CreateAnimalProps<TChildProps, TDataName>>) {
+    return ApolloReactHoc.withMutation<TProps, CreateAnimalMutation, CreateAnimalMutationVariables, CreateAnimalProps<TChildProps, TDataName>>(CreateAnimalDocument, {
+      alias: 'createAnimal',
+      ...operationOptions
+    });
+};
+export type CreateAnimalMutationResult = ApolloReactCommon.MutationResult<CreateAnimalMutation>;
+export type CreateAnimalMutationOptions = ApolloReactCommon.BaseMutationOptions<CreateAnimalMutation, CreateAnimalMutationVariables>;
+export const UpdateAnimalDocument = gql`
+    mutation UpdateAnimal($id: ID!, $species: String!, $favoriteFood: String!) {
+  updateAnimal(id: $id, species: $species, favoriteFood: $favoriteFood)
+}
+    `;
+export type UpdateAnimalMutationFn = ApolloReactCommon.MutationFunction<UpdateAnimalMutation, UpdateAnimalMutationVariables>;
+export type UpdateAnimalProps<TChildProps = {}, TDataName extends string = 'mutate'> = {
+      [key in TDataName]: ApolloReactCommon.MutationFunction<UpdateAnimalMutation, UpdateAnimalMutationVariables>
+    } & TChildProps;
+export function withUpdateAnimal<TProps, TChildProps = {}, TDataName extends string = 'mutate'>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  UpdateAnimalMutation,
+  UpdateAnimalMutationVariables,
+  UpdateAnimalProps<TChildProps, TDataName>>) {
+    return ApolloReactHoc.withMutation<TProps, UpdateAnimalMutation, UpdateAnimalMutationVariables, UpdateAnimalProps<TChildProps, TDataName>>(UpdateAnimalDocument, {
+      alias: 'updateAnimal',
+      ...operationOptions
+    });
+};
+export type UpdateAnimalMutationResult = ApolloReactCommon.MutationResult<UpdateAnimalMutation>;
+export type UpdateAnimalMutationOptions = ApolloReactCommon.BaseMutationOptions<UpdateAnimalMutation, UpdateAnimalMutationVariables>;
+export const DeleteAnimalDocument = gql`
+    mutation DeleteAnimal($id: ID!) {
+  deleteAnimal(id: $id)
+}
+    `;
+export type DeleteAnimalMutationFn = ApolloReactCommon.MutationFunction<DeleteAnimalMutation, DeleteAnimalMutationVariables>;
+export type DeleteAnimalProps<TChildProps = {}, TDataName extends string = 'mutate'> = {
+      [key in TDataName]: ApolloReactCommon.MutationFunction<DeleteAnimalMutation, DeleteAnimalMutationVariables>
+    } & TChildProps;
+export function withDeleteAnimal<TProps, TChildProps = {}, TDataName extends string = 'mutate'>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  DeleteAnimalMutation,
+  DeleteAnimalMutationVariables,
+  DeleteAnimalProps<TChildProps, TDataName>>) {
+    return ApolloReactHoc.withMutation<TProps, DeleteAnimalMutation, DeleteAnimalMutationVariables, DeleteAnimalProps<TChildProps, TDataName>>(DeleteAnimalDocument, {
+      alias: 'deleteAnimal',
+      ...operationOptions
+    });
+};
+export type DeleteAnimalMutationResult = ApolloReactCommon.MutationResult<DeleteAnimalMutation>;
+export type DeleteAnimalMutationOptions = ApolloReactCommon.BaseMutationOptions<DeleteAnimalMutation, DeleteAnimalMutationVariables>;
+export const GetAnimalDocument = gql`
+    query GetAnimal($id: ID!) {
+  animal(id: $id) {
+    id
+    species
+    favoriteFood
+  }
+}
+    `;
+export type GetAnimalProps<TChildProps = {}, TDataName extends string = 'data'> = {
+      [key in TDataName]: ApolloReactHoc.DataValue<GetAnimalQuery, GetAnimalQueryVariables>
+    } & TChildProps;
+export function withGetAnimal<TProps, TChildProps = {}, TDataName extends string = 'data'>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  GetAnimalQuery,
+  GetAnimalQueryVariables,
+  GetAnimalProps<TChildProps, TDataName>>) {
+    return ApolloReactHoc.withQuery<TProps, GetAnimalQuery, GetAnimalQueryVariables, GetAnimalProps<TChildProps, TDataName>>(GetAnimalDocument, {
+      alias: 'getAnimal',
+      ...operationOptions
+    });
+};
+export type GetAnimalQueryResult = ApolloReactCommon.QueryResult<GetAnimalQuery, GetAnimalQueryVariables>;
+export const GetAnimalsDocument = gql`
+    query GetAnimals {
+  animals {
+    id
+    species
+    favoriteFood
+  }
+}
+    `;
+export type GetAnimalsProps<TChildProps = {}, TDataName extends string = 'data'> = {
+      [key in TDataName]: ApolloReactHoc.DataValue<GetAnimalsQuery, GetAnimalsQueryVariables>
+    } & TChildProps;
+export function withGetAnimals<TProps, TChildProps = {}, TDataName extends string = 'data'>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  GetAnimalsQuery,
+  GetAnimalsQueryVariables,
+  GetAnimalsProps<TChildProps, TDataName>>) {
+    return ApolloReactHoc.withQuery<TProps, GetAnimalsQuery, GetAnimalsQueryVariables, GetAnimalsProps<TChildProps, TDataName>>(GetAnimalsDocument, {
+      alias: 'getAnimals',
+      ...operationOptions
+    });
+};
+export type GetAnimalsQueryResult = ApolloReactCommon.QueryResult<GetAnimalsQuery, GetAnimalsQueryVariables>;
